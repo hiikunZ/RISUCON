238fdf05f3ff04e24426828262e53ff5305f2f9f
hiikunZ <70016905+hiikunZ@users.noreply.github.com>
2024-01-02 13:32:52 +0900

    56052 通りまで増やした

--- bench/generator.go
+++ bench/generator.go
@@ -7,16 +7,20 @@ import (
 
 func Usergen() string {
 	nameprefix := []string{
-		"aa","ii","uu","ee","oo","kaa","kii","kuu","kee","koo","saa","sii","suu","see","soo","taa","tii","tuu","tee","too","naa","nii","nuu","nee","noo","haa","hii","huu","hee","hoo","maa","mii","muu","mee","moo","yaa","yuu","yoo","raa","rii","ruu","ree","roo","waa","woo","gaa","gii","guu","gee","goo","zaa","zii","zuu","zee","zoo","daa","dii","duu","dee","doo","baa","bii","buu","bee","boo","paa","pii","puu","pee","poo",
+		"a","i","u","e","o","ka","ki","ku","ke","ko","sa","shi","su","se","so","ta","chi","tsu","te","to","na","ni","nu","ne","no","ha","hi","fu","he","ho","ma","mi","mu","me","mo","ya","yu","yo","ra","ri","ru","re","ro","wa","wo","ga","gi","gu","ge","go","za","zi","zu","ze","zo","da","di","du","de","do","ba","bi","bu","be","bo","pa","pi","pu","pe","po",
+		"aa","ii","uu","ee","oo","kaa","kii","kuu","kee","koo","saa","shii","suu","see","soo","taa","chii","tsuu","tee","too","naa","nii","nuu","nee","noo","haa","hii","fuu","hee","hoo","maa","mii","muu","mee","moo","yaa","yuu","yoo","raa","rii","ruu","ree","roo","waa","woo","gaa","gii","guu","gee","goo","zaa","zii","zuu","zee","zoo","daa","dii","duu","dee","doo","baa","bii","buu","bee","boo","paa","pii","puu","pee","poo",
+		"kyaa","kyuu","kyoo","gyaa","gyuu","gyoo","shaa","shuu","shoo","jaa","juu","joo","chaa","chuu","choo","nyaa","nyuu","nyoo","hyaa","hyuu","hyoo","byaa","byuu","byoo","pyaa","pyuu","pyoo","myaa","myuu","myoo","ryaa","ryuu","ryoo",
 	}
 	displaynameprefix := []string{
+		"あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","を","が","ぎ","ぐ","げ","ご","ざ","じ","ず","ぜ","ぞ","だ","ぢ","づ","で","ど","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ",
 		"あー","いー","うー","えー","おー","かー","きー","くー","けー","こー","さー","しー","すー","せー","そー","たー","ちー","つー","てー","とー","なー","にー","ぬー","ねー","のー","はー","ひー","ふー","へー","ほー","まー","みー","むー","めー","もー","やー","ゆー","よー","らー","りー","るー","れー","ろー","わー","をー","がー","ぎー","ぐー","げー","ごー","ざー","じー","ずー","ぜー","ぞー","だー","ぢー","づー","でー","どー","ばー","びー","ぶー","べー","ぼー","ぱー","ぴー","ぷー","ぺー","ぽー",
+		"きゃー","きゅー","きょー","ぎゃー","ぎゅー","ぎょー","しゃー","しゅー","しょー","じゃー","じゅー","じょー","ちゃー","ちゅー","ちょー","にゃー","にゅー","にょー","ひゃー","ひゅー","ひょー","びゃー","びゅー","びょー","ぴゃー","ぴゅー","ぴょー","みゃー","みゅー","みょー","りゃー","りゅー","りょー",
 	}
 	namemiddle := []string{
-		"kun","chan","san",
+		"kun","chan","san","sama","dono","sensei","shisho","hakase","shi",
 	}
 	displaynamemiddle := []string{
-		"くん","ちゃん","さん",
+		"くん","ちゃん","さん","さま","どの","せんせい","ししょう","はかせ","し",
 	}
 	namesuffix := []string{
 		"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0",
